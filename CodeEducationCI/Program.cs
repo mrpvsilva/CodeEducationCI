6990d894d4b0865102090719dab63c5035cfbad0
Paulo Silva <mrpvsilva@gmail.com>
2022-09-17 19:50:12 -0300

    ci: fix Dockerfile

--- CodeEducationCI/Program.cs
+++ CodeEducationCI/Program.cs
@@ -1,12 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CodeEducationCI;
 
-namespace CodeEducationCI;
-public class Program
-{
-    public static void Main(string[] args)
-    {
-
-        Console.WriteLine($"Somar {Calculadora.Somar(1, 2)}");
-    }
-}
+Console.WriteLine($"Somar {Calculadora.Somar(1, 2)}");
