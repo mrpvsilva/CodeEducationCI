f18ad272cb1a57031678f69d3c41e3f558b122cf
Paulo Silva <mrpvsilva@gmail.com>
2022-09-17 18:41:46 -0300

    feat: CI com github actions

---
+++ CodeEducationCI/Calculadora.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeEducationCI
+{
+    public class Calculadora
+    {
+        public static int Somar(params int[] value)
+        {
+            return value.Sum(x => x);
+        }
+    }
+}
